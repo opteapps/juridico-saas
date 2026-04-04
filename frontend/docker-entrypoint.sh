03d3357253075199fdfaee2cc8849e72d9611693
Andre <seu-email@gmail.com>
2026-04-04 09:07:11 -0300

    fix: configura proxy reverso no nginx com URL fixa do backend

--- frontend/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Substitui a variável BACKEND_URL no nginx.conf
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Inicia o nginx
-nginx -g 'daemon off;'
